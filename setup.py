15fe3ac4b7f70eef584c1245a6136b196ee70642
Raushan Joshi <raushanjoshi199@gmail.com>
2025-08-23 20:11:06 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#
+t#
 # Copyright (C) 2023, Inria
 # GRAPHDECO research group, https://team.inria.fr/graphdeco
 # All rights reserved.
@@ -15,11 +15,11 @@ import os
 os.path.dirname(os.path.abspath(__file__))
 
 setup(
-    name="diff_gaussian_rasterization",
-    packages=['diff_gaussian_rasterization'],
+    name="object_feature_rasterization",
+    packages=['object_feature_rasterization'],
     ext_modules=[
         CUDAExtension(
-            name="diff_gaussian_rasterization._C",
+            name="object_feature_rasterization._C",
             sources=[
             "cuda_rasterizer/rasterizer_impl.cu",
             "cuda_rasterizer/forward.cu",
